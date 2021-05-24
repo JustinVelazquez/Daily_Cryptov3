0b1d715df7cbcf3eed74773fe3860b0f7eba40ec
Justin V <justinv4994@gmail.com>
2021-05-23 22:14:49 -0400

    played around with tailwind styling. will layout app tomorrow

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 
-
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World. This is Going to be crypto daily!</h1>
+    <div>
+      <ul class="flex-row">
+        <li class="text-gray-500 flex-auto">
+          Hello World. This is Going to be crypto daily!
+        </li>
+        <li class="text-yellow-500 flex-auto">
+          Hello World. This is Going to be crypto daily!
+        </li>
+      </ul>
     </div>
   );
 }
