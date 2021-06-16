cfe04518343790fcf4a7e48e5e070da5d94939ee
Justin V <justinv4994@gmail.com>
2021-06-15 23:16:46 -0400

    updated posts

--- client-side/src/components/Content/posts/Posts.js
+++ client-side/src/components/Content/posts/Posts.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getPosts } from '../../../actions/posts';
 import Post from './post/post';
